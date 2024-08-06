db0e76cde24cb7b52c7dac92a940bac7c32ec488
hikmot-kasali <kasalihikmot@gmail.com>
2024-08-06 08:21:50 +0100

    Adding

--- Password-generator/main.js
+++ Password-generator/main.js
@@ -1,4 +1,4 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","("-")","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"]
 let passwordEl = document.getElementById("password-el");
 console.log(passwordEl)
@@ -20,5 +20,6 @@ passwordButton.addEventListener("click", function () {
     for (let i = 15; i <16; i+=15) {
         passwordEl.textContent += characters[i]
         passwordEl2.textContent += characters[i]
+        console.log(renderPassword() )
     }
 }
